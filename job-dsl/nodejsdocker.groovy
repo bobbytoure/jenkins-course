b0cf1018ae4c327424f625836f61be050f0d0484
bobbytoure <bobby.toure@gmail.com>
2019-07-06 16:40:05 -0400

    update dsl repository3

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('bobbytoure/docker-nodes-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('bobbytoure')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
